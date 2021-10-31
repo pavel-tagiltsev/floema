0de623f46b99a3dc7ff8df8bbd91af7ff0a3a70b
Павел <pavel.tagiltsef@gmail.com>
2021-10-31 16:18:10 +0300

    Set up integration with Prismic CMS

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
+
+const handelLinkResolver = doc => {
+  return '/'
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handelLinkResolver
+  }
+
+  res.locals.PrismicDOM = PrismicDOM
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -11,14 +37,25 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.render('pages/about')
+  initApi(req).then(api => {
+    api.query(Prismic.Predicates.any('document.type', ['about', 'meta']))
+      .then(response => {
+        const { results } = response
+        const [about, meta] = results
+
+        res.render('pages/about', {
+          about,
+          meta
+        })
+      })
+  })
 })
 
 app.get('/collections', (req, res) => {
   res.render('pages/collections')
 })
 
-app.get('/detail/:id', (req, res) => {
+app.get('/detail/:uid', (req, res) => {
   res.render('pages/detail')
 })
 
